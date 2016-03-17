b493656128dd531b7ec64bbdca62a274fd4e852b
vladimir.burdonov <vladimir.burdonov@opensoftdev.ru>
2016-03-17 18:02:37 +0300

    Added project to fake repositories realizations.

---
+++ Marya.MeasurementManager.Repository.Fake/Orders/OrderSity.cs
@@ -0,0 +1,13 @@
+﻿using Marya.MeasurementManager.DomainModel.Orders;
+
+namespace Marya.MeasurementManager.Repository.Fake.Orders
+{
+    /// <summary>
+    /// Реализация города для хранения в репозиториях
+    /// </summary>
+    internal class OrderSity : IOrderSity
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Marya.MeasurementManager.Repository.Fake/Orders/Repositories/OrderSityRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Marya.MeasurementManager.DomainModel.Orders;
+using Marya.MeasurementManager.DomainModel.Orders.Repositories;
+
+namespace Marya.MeasurementManager.Repository.Fake.Orders.Repositories
+{
+    /// <summary>
+    /// Реализация репозитория для городов
+    /// </summary>
+    internal class OrderSityRepository : IOrderSityRepository
+    {
+        public Task<IEnumerable<IOrderSity>> GetAllAsync()
+        {
+            return Task.FromResult(new List<IOrderSity>
+            {
+                new OrderSity {Id = 1, Name = "Москва" },
+                new OrderSity {Id = 2, Name = "Санкт-Петербург" },
+                new OrderSity {Id = 3, Name = "Саратов" },
+                new OrderSity {Id = 4, Name = "Нижний Новгород" },
+                new OrderSity {Id = 5, Name = "Казань" }
+            }.OrderBy(x => x.Name).AsEnumerable());
+        }
+    }
+}
